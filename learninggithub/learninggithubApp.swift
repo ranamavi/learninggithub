c3c8a7232faf21dd5bcee8fef7842959b1ad5c85
Rana Mavi <ranaamavii@gmail.com>
2023-08-14 17:19:42 +0300

    Initial Commit

---
+++ learninggithub/learninggithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learninggithubApp.swift
+//  learninggithub
+//
+//  Created by Rana Mavi on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct learninggithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
